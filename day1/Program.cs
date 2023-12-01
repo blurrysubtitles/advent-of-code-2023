7bbe8c32459db4ccb161fcfa3b760aab6674c280
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-12-01 18:33:15 +0000

    switch to using Worker sdk directly, build isn't doing it on its own

--- day1/Program.cs
+++ day1/Program.cs
@@ -1,9 +1,7 @@
-﻿namespace DayOne;
+using day1;
 
-public class Program
-{
-    public static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
