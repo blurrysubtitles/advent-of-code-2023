c35d3ddc97e658ff917911dffb650aa77515d14c
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-12-01 19:37:03 +0000

    write basic logic for problem statement

--- DayOne/Worker.cs
+++ DayOne/Worker.cs
@@ -1,18 +1,48 @@
+using Microsoft.Extensions.Options;
+using System.Text.RegularExpressions;
+
 namespace DayOne;
 
 public class Worker : BackgroundService
 {
+    private readonly WorkerSettings _settings;
     private readonly ILogger<Worker> _logger;
 
-    public Worker(ILogger<Worker> logger)
-        => _logger = logger;
+    public Worker(IOptions<WorkerSettings> options, ILogger<Worker> logger)
+        => (_settings, _logger) = (options.Value, logger);
 
-    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    protected override async Task ExecuteAsync(CancellationToken cancellationToken)
     {
-        while(!stoppingToken.IsCancellationRequested)
+        try
         {
             _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
-            await Task.Delay(1000, stoppingToken);
+
+            using StreamReader fileReader = new(_settings.InputFilePath);
+            int lineCount = 0;
+
+            while(cancellationToken.IsCancellationRequested is false
+                && fileReader.EndOfStream is false)
+            {
+                string line = await fileReader.ReadLineAsync(cancellationToken) ?? string.Empty;
+                lineCount++;
+                MatchCollection matches = Regex.Matches(line, _settings.SingleDigitRegexPattern);
+
+                if(matches.Count == 0)
+                {
+                    _logger.LogWarning($"no matches found on line {lineCount}");
+                }
+                else
+                {
+                    string firstDigit = matches.First().Value;
+                    string lastDigit = matches.Last().Value;
+                    string lineResult = string.Concat(firstDigit, lastDigit);
+                    _logger.LogInformation($"{nameof(lineCount)}={lineCount} :: {nameof(lineResult)}={lineResult}");
+                }
+            }
+        }
+        catch(Exception exception)
+        {
+            _logger.LogError(exception.Message);
         }
     }
 }
