f7d5694d339e8a9213dc3fb67c24213618cc761c
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-12-01 18:43:24 +0000

    Worker constructor can be an expression

--- DayOne/Worker.cs
+++ DayOne/Worker.cs
@@ -5,9 +5,7 @@ public class Worker : BackgroundService
     private readonly ILogger<Worker> _logger;
 
     public Worker(ILogger<Worker> logger)
-    {
-        _logger = logger;
-    }
+        => _logger = logger;
 
     protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
