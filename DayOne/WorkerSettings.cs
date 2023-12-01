7689f7638d3130c0bb3f0334aacee62983929625
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-12-01 19:36:38 +0000

    add options calls for file path, regex pattern, etc.

---
+++ DayOne/WorkerSettings.cs
@@ -0,0 +1,7 @@
+namespace DayOne;
+
+public class WorkerSettings
+{
+    public string InputFilePath {get; set; } = "./Content/input.txt";
+    public string SingleDigitRegexPattern {get; set; } = @"\d";
+}
