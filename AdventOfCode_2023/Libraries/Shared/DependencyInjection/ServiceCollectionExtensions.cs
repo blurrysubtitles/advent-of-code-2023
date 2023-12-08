2def7d01f9996484c2ced9f2b379b1b18d8eb6c7
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-12-08 04:57:12 +0000

    add project for dependency injection functionality, move over extension from app

--- AdventOfCode_2023/Libraries/Shared/DependencyInjection/ServiceCollectionExtensions.cs
+++ AdventOfCode_2023/Libraries/Shared/DependencyInjection/ServiceCollectionExtensions.cs
@@ -1,9 +1,4 @@
-using AdventOfCode.Models;
-using AdventOfCode.Services;
-
-namespace AdventOfCode.Extensions;
-
-public static class DependencyInjectionExtensions
+public static class ServiceCollectionExtensions
 {
     public static void AddDay01Worker(this IServiceCollection services)
         => services
