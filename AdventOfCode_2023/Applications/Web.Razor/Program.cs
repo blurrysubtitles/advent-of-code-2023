1a0c8c39bf772bdf6e2bbe4f5842c4fce4c4d191
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-12-09 16:54:20 +0000

    never not dev env

--- AdventOfCode_2023/Applications/Web.Razor/Program.cs
+++ AdventOfCode_2023/Applications/Web.Razor/Program.cs
@@ -13,14 +13,6 @@ public static class Program
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if(!app.Environment.IsDevelopment())
-            {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                app.UseHsts();
-            }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
