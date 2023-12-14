ba08c1685eb9f4e4448c96ebc46604c667579578
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-12-14 03:22:36 +0000

    maybe things are in an ok state to suit my ridiculous styling whims

--- scripts/startup.sh
+++
@@ -1,4 +0,0 @@
-```bash
-#!/bin/bash
-echo 'alias sln_add_all="find -name '*.csproj' -exec dotnet sln add {} \;"' >> ~/.bash_profile
-echo 'alias sln_remove_all="find -name '*.csproj' -exec dotnet sln remove {} \;"' >> ~/.bash_profile
