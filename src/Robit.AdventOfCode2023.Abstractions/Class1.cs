7522308211d27eeaf74c6c1e512fd9a03dc38d73
blurrysubtitles <81658455+blurrysubtitles@users.noreply.github.com>
2023-12-17 14:55:11 +0000

    trying a functional approach

--- src/Robit.AdventOfCode2023.Abstractions/Class1.cs
+++ src/Robit.AdventOfCode2023.Abstractions/Class1.cs
@@ -1,6 +1,21 @@
-﻿namespace Robit.AdventOfCode2023.Abstractions;
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
-public class Class1
+namespace Robit.AdventOfCode2023.Abstractions;
+
+public interface IQuery { }
+
+public interface IRepository<TModel>
+{
+    IEnumerable<TModel> Select(IQuery query, CancellationToken cancellationToken = default);
+    void Insert(IEnumerable<TModel> list, CancellationToken cancellationToken = default);
+    void Update(IEnumerable<TModel> list, CancellationToken cancellationToken = default);
+    void Delete(IEnumerable<TModel> list, CancellationToken cancellationToken = default);
+}
+
+public abstract class BaseRepository<TModel>(ILogger<BaseRepository<TModel>> Logger) { }
+
+public static class RepositoryExtensions
 {
 
 }
--- src/Robit.AdventOfCode2023.AppRazor/Program.cs
+++ src/Robit.AdventOfCode2023.AppRazor/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.Extensions.Logging.Console;
+
 namespace AdventOfCode_2023.Applications.Web.Razor;
 
 public static class Program
@@ -6,26 +8,51 @@ public static class Program
     {
         try
         {
-            WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+            WebApplicationBuilder builder =
+                WebApplication
+                .CreateSlimBuilder(args);
 
-            builder.Services.AddRazorPages();
+            WebApplication app =
+                builder
+                .Logging.AddSimpleConsole(_configureSimpleConsoleFormatterOptions).Return(builder)
+                .Services.AddRazorPages().Return(builder)
+                .Build();
 
-            WebApplication app = builder.Build();
+            Task mainTask =
+                app
+                .UseHttpsRedirection()
+                .UseStaticFiles()
+                .UseRouting()
+                .UseAuthorization().Return(app)
+                .MapRazorPages().Return(app)
+                .RunAsync();
 
-            app.UseHttpsRedirection();
-            app.UseStaticFiles();
-
-            app.UseRouting();
-
-            app.UseAuthorization();
-
-            app.MapRazorPages();
-
-            await app.RunAsync();
+            await mainTask;
         }
         catch(Exception exception)
         {
             Console.WriteLine("Main failed: {0}", exception.Message);
         }
+    }
+
+    static Action<SimpleConsoleFormatterOptions> _configureSimpleConsoleFormatterOptions = options =>
+        options
+        .Mutate(o => o.IncludeScopes = true)
+        .Mutate(o => o.SingleLine = true)
+        .ReturnVoid();
+
+    static TReturned Return<TExtended, TReturned>(
+        this TExtended _,
+        TReturned result)
+        => result;
+
+    static void ReturnVoid<TExtended>(
+        this TExtended _)
+        { }
+
+    static T Mutate<T>(this T result, Action<T> action) where T : class
+    {
+        action.Invoke(result);
+        return result;
     }
 }
